47a1830fd64415abef060a977daf7b6752aad5fe
ikkerxd <090229@unsaac.edu.pe>
2016-03-16 09:46:48 -0500

    creacion de carpetas por cada app, crecacion templates base, template login (modificacion users.views)

--- nhaku/apps/users/views.py
+++ nhaku/apps/users/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class LogIn(TemplateView):
+    template_name='users/login/login.html'
