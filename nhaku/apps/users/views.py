a3c4b7cf24839bc35df988a298e239b4141f37fe
ikkerxd <090229@unsaac.edu.pe>
2016-03-16 11:28:24 -0500

    fin del diseño de login y sincronia con el forms.py

--- nhaku/apps/users/views.py
+++ nhaku/apps/users/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
 
+from forms import LoginForm
 
-class LogIn(TemplateView):
-    template_name='users/login/login.html'
+class LogIn(FormView):
+    template_name = 'users/login/login.html'
+    form_class = LoginForm
+
+    def form_valid(self, form):
+        return super(LogIn, self).form_valid(form)
