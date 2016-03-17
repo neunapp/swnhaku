d8fb9edcca9316ff82356d462a1c5d5134bf2e54
dgafz <scam1121@gmail.com>
2016-03-17 17:13:14 -0500

    manteniminto clientes

--- nhaku/applications/profiles/urls.py
+++ nhaku/applications/profiles/urls.py
@@ -2,9 +2,29 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # url(
-    #     r'^panel/nota-ingreso/$',
-    #     views.DepositSlipView.as_view(),
-    #     name='nota-ingreso'
-    # ),
+    url(
+        r'^perfiles/cliente/list$',
+        views.ClientListView.as_view(),
+        name='cliente-list'
+    ),
+    url(
+        r'^perfiles/cliente/add$',
+        views.ClientRegister.as_view(),
+        name='cliente-add'
+    ),
+    url(
+        r'^perfiles/cliente/update/(?P<pk>\d+)/$',
+        views.ClientUpdateView.as_view(),
+        name='cliente-update'
+    ),
+    url(
+        r'^perfiles/cliente/detail/(?P<pk>\d+)/$',
+        views.ClientDetailView.as_view(),
+        name='cliente-detail'
+    ),
+    url(
+        r'^perfiles/cliente/delete/(?P<pk>\d+)/$',
+        views.ClientDeleteView.as_view(),
+        name='cliente-delete'
+    ),
 ]
--- nhaku/applications/profiles/views.py
+++ nhaku/applications/profiles/views.py
@@ -1,3 +1,115 @@
+# -*- encoding: utf-8 -*-
 from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
+from django.views.generic import (
+    CreateView,
+    UpdateView,
+    DetailView,
+    DeleteView,
+    ListView,
+    TemplateView,
+)
+from django.views.generic.edit import FormView
+
+from applications.users.models import User
+
+from .models import Client
+
+from .forms import ClientForm, ClientUpdateForm
 
 # Create your views here.
+
+#mantenimiento para Clientes
+class ClientRegister(FormView):
+    '''
+        vista para registrar clientes
+    '''
+    template_name = 'profiles/client/add.html'
+    form_class = ClientForm
+    success_url = reverse_lazy('profiles_app:cliente-list')
+
+    def form_valid(self, form):
+        #registramos usuario
+        usuario = User.objects.create_user(
+            username=form.cleaned_data['user_name'],
+            password=form.cleaned_data['password2'],
+            type_user='3',
+        )
+        usuario.save()
+        #registramos el cliente
+        cliente = Client(
+            user=usuario,
+            full_name=form.cleaned_data['full_name'],
+            number_doc=form.cleaned_data['number_doc'],
+            address=form.cleaned_data['address'],
+            email=form.cleaned_data['email'],
+            phone=form.cleaned_data['phone'],
+            web=form.cleaned_data['web'],
+            avatar=form.cleaned_data['avatar'],
+            type_clint=form.cleaned_data['type_clint'],
+            user_created=self.request.user,
+        )
+        cliente.save()
+
+        return super(ClientRegister, self).form_valid(form)
+
+
+class ClientUpdateView(UpdateView):
+    '''
+        vista para actualizar datos de cliente
+    '''
+    model = Client
+    template_name = 'profiles/client/update.html'
+    form_class = ClientUpdateForm
+    success_url = reverse_lazy('profiles_app:cliente-list')
+
+    def form_valid(self, form):
+        #recuperamos y actualizamos usuario de modificacion
+        cliente = self.get_object()
+        cliente.user_modified = self.request.user
+        cliente.save()
+
+        return super(ClientUpdateView, self).form_valid(form)
+
+
+class ClientDetailView(DetailView):
+    '''
+        vista para mostra los datos de un cliente en detalle
+    '''
+    model = Client
+    template_name = 'profiles/client/detail.html'
+
+
+class ClientDeleteView(DetailView):
+    '''
+    Eliminar Client.
+    '''
+    model = Client
+    template_name = 'profiles/client/delete.html'
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        # recuperamos el objeto y actualizamos a anulado
+        cliente = self.object
+        cliente.state = True
+        cliente.save()
+        return HttpResponseRedirect(
+            reverse(
+                'profiles_app:cliente-list'
+            )
+        )
+
+
+class ClientListView(ListView):
+    '''
+        muestra la lista de clientes no eliminados
+    '''
+    context_object_name = 'client_list'
+    template_name = 'profiles/client/list.html'
+    paginate_by = 20
+
+    def get_queryset(self):
+        #recuperamos el valor por GET
+        queryset = Client.objects.filter(state=False)
+        return queryset
